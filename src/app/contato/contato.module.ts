b346476014c3b0ed58ef0da6d563ece7a35d46fa
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2023-01-26 11:24:58 -0300

    Implementação da funcionalidade barra de pesquisa

--- src/app/contato/contato.module.ts
+++ src/app/contato/contato.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ContatoComponent } from './contato.component';
   ],
   imports: [
     CommonModule,
-    ContatoRoutingModule
+    ContatoRoutingModule,
+    ReactiveFormsModule 
   ]
 })
 export class ContatoModule { }
